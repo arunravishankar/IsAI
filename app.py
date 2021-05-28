9d4f3cfb367264e559286a1d0a245b44f6077301
Arun Ravishankar <arunravishankar@gmail.com>
2021-05-28 11:14:30 -0700

    Post request app.py
    - Added a POST request method to upload files and save it in a directory

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 from flask import Flask, app, render_template, request, redirect, url_for, abort, send_from_directory
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['UPLOAD_PATH'] = 'uploads'
+app.config['UPLOAD_EXTENSIONS'] = ['.wav', '.mp3']
+app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
+
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def upload_files():
+    uploaded_file = request.files['audiofile']
+    filename = secure_filename(uploaded_file.filename)
+    uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+    return render_template('index.html')
+
+
+@app.route('/uploads/<filename>')
+def upload(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
